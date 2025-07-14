bbd75d7777894bbba77976b686c7824c5facd62c
Rodrigo Parrilla Mesas <rodrigoparri@gmail.com>
2025-07-14 15:11:10 +0200

    GUI is mostly complete. align boxes. functionality must be added to remaining buttons. calculate MIG etc. save system not implemented

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 import sys
 import numpy as np
 from PySide6.QtWidgets import (
-    QApplication ,QWidget, QMainWindow, QHBoxLayout, QVBoxLayout, QFormLayout,
-    QSpinBox, QDoubleSpinBox, QLabel, QPushButton
+    QApplication ,QWidget, QMainWindow, QHBoxLayout, QVBoxLayout, QFormLayout, QGridLayout,
+    QSpinBox, QDoubleSpinBox, QLabel, QPushButton, QGroupBox
 )
 from PySide6.QtGui import QImage, QPixmap
 from PySide6.QtCore import Qt
 from speckle_generator import image_speckle
+
+GROUP_BOX_STYLESHEET = """
+                   QGroupBox {
+                       border: 1px solid gray;
+                       border-radius: 3px;
+                       margin-top: 10px;
+                   }
+                   QGroupBox::title {
+                       subcontrol-origin: margin;
+                       left: 10px;
+                       padding: 0 3px 0 3px;
+                   }
+               """
 
 class ImageWidget(QWidget):
 
@@ -56,8 +69,19 @@ class ParameterWidget(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        self.data_box = QGroupBox("Data")
+        self.data_box.setMaximumHeight(250)
+        self.data_box.setStyleSheet(GROUP_BOX_STYLESHEET)
+
+        self.generate_box = QGroupBox("Generate")
+        self.generate_box.setMaximumHeight(70)
+        self.generate_box.setStyleSheet(GROUP_BOX_STYLESHEET)
+        self.generate_layout = QHBoxLayout()
+
         self.layout = QFormLayout()
-        # dictionary containing all values
         self.default_values = {
             "height" : 50,
             "width" : 25,
@@ -99,13 +123,14 @@ class ParameterWidget(QWidget):
         self.rand_position_widget.setRange(0, 100)
         self.rand_position_widget.setValue(self.default_values["rand_pos"])
         # regenerate button
-        self.regen_widget = QPushButton()
-        self.regen_widget.setText("Regen")
+        self.regen_widget = QPushButton("Apply")
         self.regen_widget.setFixedSize(100,30)
         # invert button
-        self.invert_widget = QPushButton()
-        self.invert_widget.setText("Invert")
-        self.invert_widget.setFixedSize(100,30)
+        self.invert_widget = QPushButton("Invert")
+        self.invert_widget.setFixedSize(100, 30)
+        #Load defaults button
+        self.defaults_button = QPushButton("Set defaults")
+        self.defaults_button.setFixedSize(100, 30)
 
         self.layout.addRow(" Height (mm)", self.height_widget)
         self.layout.addRow(" Width (mm)", self.width_widget)
@@ -114,9 +139,18 @@ class ParameterWidget(QWidget):
         self.layout.addRow(" Grid step (% of diameter)", self.grid_step_widget)
         self.layout.addRow(" Minimum diameter (% of diameter)", self.min_diameter_widget)
         self.layout.addRow(" Position randomness (% of diameter)", self.rand_position_widget)
-        self.layout.addRow(self.invert_widget,self.regen_widget)
 
-        self.setLayout(self.layout)
+        self.generate_layout.addWidget(self.invert_widget)
+        self.generate_layout.addWidget(self.regen_widget)
+        self.generate_layout.addWidget(self.defaults_button)
+
+        self.data_box.setLayout(self.layout)
+        self.generate_box.setLayout(self.generate_layout)
+
+        self.main_layout.addWidget(self.data_box)
+        self.main_layout.addWidget(self.generate_box)
+
+        self.setLayout(self.main_layout)
 
     def get_values(self):
         self.values = {
@@ -131,6 +165,60 @@ class ParameterWidget(QWidget):
         return self.values
 
 
+class ResultsWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        self.results_box = QGroupBox("Results")
+        self.results_box.setMaximumHeight(150)
+
+        self.results_layout = QGridLayout()
+
+        self.speckle_density_label = QLabel("Density:")
+        self.MIG_label = QLabel("MIG:")
+        self.autocorrelation_map = QLabel("Autocorrelation Map")
+        self.speckle_density_result_label = QLabel("%")
+        self.MIG_result_label = QLabel("31")
+        self.autocorrelation_map_generate_button = QPushButton("Generate")
+
+        self.results_layout.addWidget(self.speckle_density_label, 0, 0)
+        self.results_layout.addWidget(self.MIG_label, 1, 0)
+        self.results_layout.addWidget(self.autocorrelation_map, 2, 0)
+        self.results_layout.addWidget(self.speckle_density_result_label, 0, 1)
+        self.results_layout.addWidget(self.MIG_result_label, 1, 1)
+        self.results_layout.addWidget(self.autocorrelation_map_generate_button, 2, 1)
+
+        self.results_box.setLayout(self.results_layout)
+        self.main_layout.addWidget(self.results_box)
+
+        self.setLayout(self.main_layout)
+
+
+class SaveWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.main_layout = QHBoxLayout()
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.save_group = QGroupBox("Save")
+        self.save_layout = QHBoxLayout()
+
+        self.save_button = QPushButton("Save as")
+        self.save_params = QPushButton("Save Parameters")
+
+        self.save_layout.addWidget(self.save_button)
+        self.save_layout.addWidget(self.save_params)
+
+        self.save_group.setLayout(self.save_layout)
+
+        self.main_layout.addWidget(self.save_group)
+
+        self.setLayout(self.main_layout)
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
@@ -139,11 +227,13 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Speckle Generator - Rodrigo Parrilla")
 
         self.main_widget = QWidget()
-        self.main_layout = QHBoxLayout(self.main_widget)
+        self.main_layout = QGridLayout(self.main_widget)
 
         self.data = ParameterWidget()
-        self.values = self.gather_values()
+        self.results = ResultsWidget()
+        self.save = SaveWidget()
 
+        self.values = self.gather_values()
        #Create first image with defaults
         self.image = ImageWidget(
             image_speckle(
@@ -160,11 +250,13 @@ class MainWindow(QMainWindow):
         self.is_inverted = False
         self.wire_connections()
 
-        self.main_layout.addWidget(self.data, 1)
-        self.main_layout.addWidget(self.image, 4)
+
+        self.main_layout.addWidget(self.data, 0, 0)
+        self.main_layout.addWidget(self.image, 0, 1, 3, 1)
+        self.main_layout.addWidget(self.results, 1, 0)
+        self.main_layout.addWidget(self.save, 2, 0)
 
         self.setCentralWidget(self.main_widget)
-
 
     def wire_connections(self):
         self.data.regen_widget.clicked.connect(self.update_image)
